8d85dd00a2d7242e24c05d3aee5cf8f1409e4817
Marinadotcom <115360016+Marinadotcom@users.noreply.github.com>
2023-06-23 00:19:26 +0530

    made some change

--- php/docprof.php
+++ php/docprof.php
@@ -24,7 +24,7 @@ if (isset($_POST['logout'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <style>
         body {
-            background: linear-gradient(to bottom, #e0e0e0, #999999);
+            background: #001c30;
         }
 
         table.table-bordered {
@@ -37,60 +37,83 @@ if (isset($_POST['logout'])) {
             background-color: rgba(255, 255, 255, 0.8);
             border: none;
         }
+
+        .container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+        }
+
+        .profile-container {
+            background-color: rgba(255, 255, 255, 0.8);
+            padding: 20px;
+            border-radius: 5px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            text-align: center;
+        }
+
+        .profile-container h3 {
+            margin-bottom: 20px;
+        }
+
+        .button-container {
+            margin-top: 20px;
+            display: flex;
+            justify-content: center;
+        }
+
+        .button-container form {
+            margin: 0 10px;
+        }
     </style>
 </head>
 
 <body>
-<div class="container">
-    <div class="row justify-content-center mt-5">
-        <div class="col-md-6">
-                <h3 class="text-center mb-4">DOCTOR PROFILE</h3>
-                <table class="table table-bordered">
-                    <tbody>
-                        <tr>
-                            <th>NAME</th>
-                            <td><?php echo $_SESSION['doctorName']; ?></td>
-                        </tr>
-                        <tr>
-                            <th>AGE</th>
-                            <td><?php echo $_SESSION['age']; ?></td>
-                        </tr>
-                        <tr>
-                            <th>GENDER</th>
-                            <td><?php echo $_SESSION['gender']; ?></td>
-                        </tr>
-                        <tr>
-                            <th>DEPARTMENT</th>
-                            <td><?php echo $_SESSION['department']; ?></td>
-                        </tr>
-                        <tr>
-                            <th>EXPERIENCE</th>
-                            <td><?php echo $_SESSION['experience']; ?></td>
-                        </tr>
-                        <tr>
-                            <th>PHONE NO</th>
-                            <td><?php echo $_SESSION['phone']; ?></td>
-                        </tr>
-                        <tr>
-                            <th>EMAIL</th>
-                            <td><?php echo $_SESSION['email']; ?></td>
-                        </tr>
-                    </tbody>
-                </table>
+    <div class="container">
+        <div class="profile-container">
+            <h3>DOCTOR PROFILE</h3>
+            <table class="table table-bordered">
+                <tbody>
+                    <tr>
+                        <th>NAME</th>
+                        <td><?php echo $_SESSION['doctorName']; ?></td>
+                    </tr>
+                    <tr>
+                        <th>AGE</th>
+                        <td><?php echo $_SESSION['age']; ?></td>
+                    </tr>
+                    <tr>
+                        <th>GENDER</th>
+                        <td><?php echo $_SESSION['gender']; ?></td>
+                    </tr>
+                    <tr>
+                        <th>DEPARTMENT</th>
+                        <td><?php echo $_SESSION['department']; ?></td>
+                    </tr>
+                    <tr>
+                        <th>EXPERIENCE</th>
+                        <td><?php echo $_SESSION['experience']; ?></td>
+                    </tr>
+                    <tr>
+                        <th>PHONE NO</th>
+                        <td><?php echo $_SESSION['phone']; ?></td>
+                    </tr>
+                    <tr>
+                        <th>EMAIL</th>
+                        <td><?php echo $_SESSION['email']; ?></td>
+                    </tr>
+                </tbody>
+            </table>
 
+            <div class="button-container">
                 <form method="POST" action="../addrecord.html">
-                <button type="submit" class="btn btn-primary">Add Patient Records</button>
-            </form>
-
+                    <button type="submit" class="btn btn-primary">Add Patient Records</button>
+                </form>
 
                 <form method="POST">
-                    <button type="submit" name="logout" class="btn btn-primary">Logout</button>
-                </form>
-            </div>
-        </div>
-    </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-</body>
-
-</html>
+                    <button type="submit" name="logout" class
